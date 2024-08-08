ae58d293f5c655a79624a590f099c0a6e9ce39a8
zzzzz <ricardopinto15@hotmail.com>
2024-08-08 11:41:04 +0100

    2 fixes

--- batch.py
+++ batch.py
@@ -5,7 +5,7 @@ import torch
 
 class Batch(ctypes.Structure):
     _fields_ = [
-        ('num_active_features', ctypes.c_uint),
+        ('num_active_features', ctypes.c_int),
         ('active_features_white_stm', ctypes.POINTER(ctypes.c_int)),
         ('active_features_black_stm', ctypes.POINTER(ctypes.c_int)),
         ('is_white_stm', ctypes.POINTER(ctypes.c_bool)),
@@ -15,8 +15,6 @@ class Batch(ctypes.Structure):
     ]
 
     def features_dense_tensor(self, white_stm: bool):
-        features_tensor = torch.zeros(BATCH_SIZE, 768 * INPUT_BUCKETS, device=device)
-
         arr = np.ctypeslib.as_array(
             self.active_features_white_stm if white_stm else self.active_features_black_stm,
             shape=(BATCH_SIZE, 64 if FACTORIZER else 32)
